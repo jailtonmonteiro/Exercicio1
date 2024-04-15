71ed038bd5fdc1c1c29b826fe86f9a9abb32d1ae
Jailton Monteiro <jailtonmonteiro147@gmail.com>
2024-04-15 10:55:52 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, s;
+
+
+            Console.WriteLine("Digite o valor 1: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor 2: ");
+            b = int.Parse(Console.ReadLine());
+
+            s = a + b;
+
+            Console.WriteLine("Soma = " + s);
+        }
+    }
+}
